3e094f850ea5dd15ea22b25e0877aa748b6dbcfd
SamIlias <Samovichilias19life@gmail.com>
2025-05-03 22:07:39 +0300

    add: weatherCard, weatherApi

---
+++ src/api/weatherAPI/wheatherApi.ts
@@ -0,0 +1,46 @@
+import { fetchWeatherApi } from 'openmeteo';
+import { getCoordinates } from '../../utils/getCoordinates.ts';
+
+const url = 'https://api.open-meteo.com/v1/forecast';
+
+export const fetchWeather = async (city: string) => {
+  const { latitude, longitude, country, name } = await getCoordinates(city);
+
+  const params = {
+    latitude,
+    longitude,
+    current: 'temperature_2m,weather_code,wind_speed_10m,wind_direction_10m',
+    // hourly: 'temperature_2m,precipitation',
+    // daily: 'weather_code,temperature_2m_max,temperature_2m_min',
+  };
+
+  const responses = await fetchWeatherApi(url, params);
+  const response = responses[0];
+  const current = response.current()!;
+  const utcOffsetSeconds = response.utcOffsetSeconds();
+
+  return {
+    current: {
+      time: new Date((Number(current.time()) + utcOffsetSeconds) * 1000),
+      temperature: current.variables(0)!.value().toFixed(1),
+      weatherCode: current.variables(1)!.value(),
+      windSpeed: current.variables(2)!.value().toFixed(1),
+      windDirection: current.variables(3)!.value().toFixed(1),
+    },
+    country: country,
+    cityName: name,
+  };
+};
+
+// types ----------------------------------------
+export type WeatherDataType = {
+  current: {
+    time: Date;
+    temperature: string;
+    weatherCode: number;
+    windSpeed: string;
+    windDirection: string;
+  };
+  country: string;
+  cityName: string;
+};
--- src/components/cards/WeatherCard.tsx
+++ src/components/cards/WeatherCard.tsx
@@ -1,42 +1,54 @@
-import {useCallback, useEffect, useState} from 'react'
-import {fetchQuote} from "../../api/quotesApi.ts";
+import * as React from 'react';
+import { CitySearchForm } from '../CitySearchForm.tsx';
+import { useEffect, useState } from 'react';
+import { fetchWeather, WeatherDataType } from '../../api/weatherAPI/wheatherApi.ts';
+import { weatherCodes, WeatherCodesType } from '../../api/weatherAPI/weatherCodes.ts';
 
-const WeatherCard = () => {
-    const [quote, setQuote] = useState<string | null>(null);
+const INITIAL_CITY = import.meta.env.VITE_CURRENT_CITY;
 
-    const loadQuote = useCallback(async () => {
-        const quote: string = await fetchQuote();
-        setQuote(quote);
-    }, [])
+const WeatherCard: React.FC = () => {
+  const [weatherData, setWeatherData] = useState<WeatherDataType>();
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
 
-    useEffect(() => {
-        let ignore = false;
+  const weatherCode = String(weatherData?.current.weatherCode) as WeatherCodesType;
 
-        const startFetchingQuote = async () => {
-            const quote: string = await fetchQuote();
-            if (!ignore) {
-                console.log("Quote is setting")
-                setQuote(quote);
-            }
-        }
+  const loadWeatherData: (cityName: string) => Promise<void> = async (cityName) => {
+    try {
+      const data: WeatherDataType = await fetchWeather(cityName);
+      setWeatherData(data);
+      setErrorMessage(null);
+    } catch (err: unknown) {
+      if (err instanceof Error) {
+        setErrorMessage(err.message);
+      }
+    }
+  };
 
-        startFetchingQuote()
+  useEffect(() => {
+    loadWeatherData(INITIAL_CITY);
+  }, []);
 
-        return () => {
-            ignore = true;
-        }
-    }, [])
+  const onSubmit = async (cityName: string) => {
+    loadWeatherData(cityName);
+  };
 
-    return (
-        <div className="card">
-            <div className="quote">
-                {quote}
-            </div>
-            <div>
-                <button className="button" onClick={loadQuote}>Refresh</button>
-            </div>
+  return (
+    <>
+      {weatherData && (
+        <div>
+          <p>Country: {weatherData.country}</p>
+          <p>Current city: {weatherData.cityName}</p>
+          <p>Temperature: {weatherData.current.temperature}</p>
+          <p>Wind Speed: {weatherData.current.windSpeed}</p>
+          <img src={weatherCodes[weatherCode].day.image} alt={weatherCode} />
         </div>
-    )
-}
+      )}
+
+      <CitySearchForm onSubmit={onSubmit} />
+
+      {errorMessage && <p className={'text-red-700'}>{errorMessage}</p>}
+    </>
+  );
+};
 
 export default WeatherCard;
