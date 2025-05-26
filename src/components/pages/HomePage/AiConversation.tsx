5573ff6652047acfd8ebc708ddccb34fcf1ba9f2
SamIlias <Samovichilias19life@gmail.com>
2025-05-26 21:59:45 +0300

    add: preloader on Home page, install: remark-gfm

--- src/components/pages/HomePage/AiConversation.tsx
+++ src/components/pages/HomePage/AiConversation.tsx
@@ -5,7 +5,10 @@ import { truncateArrayKeepFirst } from '../../../utils/truncateArrayKeepFirst.ts
 import { uniqueId } from '../../../utils/uniqueId.ts';
 import ReactMarkdown from 'react-markdown';
 import rehypeHighlight from 'rehype-highlight';
-import 'highlight.js/styles/vs2015.css'; // dark
+import 'highlight.js/styles/vs2015.css';
+import preloader from '../../../assets/preloaderGear.svg';
+import { Preloader } from '../../common/Preloader.tsx';
+import remarkGfm from 'remark-gfm'; // dark
 // import 'highlight.js/styles/vs.css'; // light
 
 export const AiConversation: React.FC = () => {
@@ -62,7 +65,11 @@ export const AiConversation: React.FC = () => {
                 ref={isLast ? lastConversationItem : null}
               >
                 <p className="italic text-amber-500">{item.role}</p>
-                <ReactMarkdown rehypePlugins={[rehypeHighlight]}>{item.content}</ReactMarkdown>
+                <div className="pl-2">
+                  <ReactMarkdown rehypePlugins={[rehypeHighlight, remarkGfm]}>
+                    {item.content}
+                  </ReactMarkdown>
+                </div>
               </div>
             );
           })
@@ -72,12 +79,19 @@ export const AiConversation: React.FC = () => {
       </div>
 
       <form onSubmit={handleSubmit} className="grid grid-rows-[5fr_1fr] gap-2 p-2">
-        <textarea
-          className="w-full p-2 border rounded resize-none focus:outline-none focus:ring-2 focus:ring-amber-500"
-          placeholder="Type your message..."
-          value={query}
-          onChange={(e) => setQuery(e.target.value)}
-        />
+        <div className="relative">
+          <textarea
+            className="w-full h-full p-2 border rounded resize-none focus:outline-none focus:ring-2 focus:ring-amber-500"
+            placeholder="Type your message..."
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+          />
+          {isSending && (
+            <div className="absolute top-0 left-0 w-full h-full bg-amber-800/40 backdrop-blur-sm flex items-center justify-center">
+              <Preloader preloader={preloader} />
+            </div>
+          )}
+        </div>
         <button
           type="submit"
           disabled={isSubmitDisabled}
