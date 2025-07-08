dbb1ca1f80f7face9a4a60309af4e26be52d913b
SamIlias <Samovichilias19life@gmail.com>
2025-07-08 22:16:42 +0300

    remove *.d.ts from netlify/functions

--- netlify/functions/askModel.d.ts
+++
@@ -1,9 +0,0 @@
-import { Handler } from '@netlify/functions';
-export type ConversationItem = {
-    role: 'system' | 'user' | 'assistant';
-    content?: string | null;
-    id: string;
-};
-export declare const initialConversationItem: ConversationItem;
-declare const handler: Handler;
-export { handler };
--- netlify/functions/fetchQuote.d.ts
+++
@@ -1,8 +0,0 @@
-import { Handler } from '@netlify/functions';
-export type QuoteType = {
-    quote: string;
-    author: string;
-    category: string;
-};
-declare const handler: Handler;
-export { handler };
